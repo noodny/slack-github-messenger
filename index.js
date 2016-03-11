e0c4a175b798ea220017d9f3c16feac09ec4d7e0
kjakubik <krzysiek.jakubik@gmail.com>
2016-03-11 15:17:15 +0100

    Fix users overwriting

--- index.js
+++ index.js
@@ -100,7 +100,9 @@ DB.connect(function() {
                             events: user.events
                         };
 
-                        users = _.reject(users, user);
+                        users = _.filter(users, function(o) {
+                            return o.id !== user.id
+                        });
                         users.push(updated);
 
                         return DB.update(updated, function() {
@@ -142,7 +144,9 @@ DB.connect(function() {
                                 events: _.union(user.events, events)
                             };
 
-                            users = _.reject(users, user);
+                            users = _.filter(users, function(o) {
+                                return o.id !== user.id
+                            });
                             users.push(updated);
 
                             return DB.update(updated, function() {
@@ -177,7 +181,9 @@ DB.connect(function() {
                                 events: _.without.apply(_, [user.events].concat(events))
                             };
 
-                            users = _.reject(users, user);
+                            users = _.filter(users, function(o) {
+                                return o.id !== user.id
+                            });
                             users.push(updated);
 
                             return DB.update(updated, function() {
